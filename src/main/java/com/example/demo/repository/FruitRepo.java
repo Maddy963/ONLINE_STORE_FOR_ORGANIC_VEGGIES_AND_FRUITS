15cba2a67c269a83497eebff1d145456cebb3614
Maddy963 <madhavan090603@gmail.com>
2023-08-23 11:15:43 +0530

    first commit

---
+++ src/main/java/com/example/demo/repository/FruitRepo.java
@@ -0,0 +1,10 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.FruitModel;
+
+public interface FruitRepo extends JpaRepository<FruitModel,Integer>
+{
+	
+}
